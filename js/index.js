cef6febd2db6403311af6dc9ce8958d65fca014a
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-05-26 14:09:52 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,5 @@
 // Vue
 
-// Darkmode
-function addDarkmodeWidget() {
-    new Darkmode().showWidget();
-    const options = {
-        bottom: '75px', // default: '32px'
-        right: '20px', // default: '32px'
-        label: '🌓', // default: ''
-    }
-    const darkmode = new Darkmode(options);
-    darkmode.showWidget();
-}
-window.addEventListener('load', addDarkmodeWidget);
-
 // APlayer
 var url="https://cdn.jsdelivr.net/gh/CYYHH3/Music";
 const ap = new APlayer({
